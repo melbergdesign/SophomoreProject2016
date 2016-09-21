99cec8377d9cc2a3577c42d7e0b3cec83c771b39
Mel Berg <melbergdesign@gmail.com>
2016-09-21 13:56:00 -0600

    Repeating Terrain
    Not Yet Randomized

--- StarCatcher/Assets/Scripts/Level/RecycleLevelObject.cs
+++ StarCatcher/Assets/Scripts/Level/RecycleLevelObject.cs
@@ -1,20 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
+//gives access to lists, preferred over arrays
+using System.Collections.Generic;
+using System;
 
 public class RecycleLevelObject : MonoBehaviour {
 
     private Vector3 newLocation;
+    public List<Recylcer> recyclableList;
+    private int i = 0;
 
+    //subscribe to the list's action
     void Start()
     {
-        StaticVars.nextSectionPosition = 12;
+        //creates a new object in memory
+        recyclableList = new List<Recylcer>();
+        Recylcer.RecycleAction += RecycleActionHandler;
+
+
+        //StaticVars.nextSectionPosition = 12;
+    }
+
+    private void RecycleActionHandler(Recylcer _r)
+    {
+        //adds each cube to list of items that can be recycled
+        recyclableList.Add(_r);
     }
 
     void OnTriggerEnter()
     {
         newLocation.x = StaticVars.nextSectionPosition;
-        transform.position = newLocation;
+        recyclableList[i].cube.position = newLocation;
         StaticVars.nextSectionPosition += StaticVars.distance;
+
+        if (i < recyclableList.Count-1)
+        {
+            i++;
+        }
 
     }
 }
