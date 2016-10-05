3a0ae2767bffa2193390a17a51c9c6b80727d5d2
Unknown <melbergdesign@gmail.com>
2016-10-05 13:54:40 -0600

    Advanced Character Flip Day
    Lost easy day scripts, must rebuild

---
+++ StarCatcher/Assets/Scripts/Character/CharacterFlipArt.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterFlipArt : MonoBehaviour
+{
+
+    public Transform characterArt;
+    public bool forward = true;
+
+    /*void FlipCharacter(bool _b)
+    {
+        characterArt.Rotate(0, 180, 0);
+        forward = _b;
+    }*/
+
+    void FlipCharacter(KeyCode _keycode)
+    {
+        switch (_keycode)
+        {
+            case KeyCode.RightArrow:
+                print("Right");
+                break;
+        }
+
+        characterArt.Rotate(0, 180, 0);
+    }
+    void Start()
+    {
+        UserInputs.UserInput += FlipCharacter;
+    }
+
+    /*void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && forward)
+        {
+            FlipCharacter(false);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) && !forward)
+        {
+            FlipCharacter(true);
+        }
+    }*/
+}
---
+++ StarCatcher/Assets/Scripts/Character/UserInputs.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class UserInputs : MonoBehaviour {
+
+    public static Action<KeyCode> UserInput;
+
+		
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.RightArrow) && UserInput !=null)
+        {
+            UserInput(KeyCode.RightArrow);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && UserInput != null)
+        {
+            UserInput(KeyCode.LeftArrow);
+        }
+    }
+}
