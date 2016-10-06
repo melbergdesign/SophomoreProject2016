dc429a1eca4ad53d8291ecee7109642af17ed768
melberg <melbergdesign@gmail.com>
2016-10-06 13:11:36 -0600

    Rebuilding the project files
    Project files were messy and confusing to work with. Spent class time redoing and commenting on scripts to make sure I understood them.

---
+++ Star Catcher Prototypes/Assets/Scripts/Character/MoveChar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveChar : MonoBehaviour {
+
+	//Creates a new instance of the Character Controller for this object
+	//Character Controller is a built-in component of Unity
+	private CharacterController myCC;
+	private Vector3 tempPos;
+
+
+	// Use this for initialization
+	void Start () {
+
+		//This "finds" the Character Controller component
+		//GetComponent is the primary way of accessing other components 
+		myCC = GetComponent<CharacterController> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Star Catcher Prototypes/Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour {
+
+	//accessible number to control speed of the camera, 'tweakable' in the unity window
+	public float speed = 1;
+	private Vector3 tempPos;
+
+		
+	// Update is called once per frame
+	void Update () {
+
+		// Assigns new value to the x coordinate of the object's vector3 
+		// Time.deltaTime forces the scene to update at a set rate rather than based on the FPS
+		// Time continues to increase from the start of the game, multiplied by the speed value to evenly increment the distance
+		// Since this is in Update, there are no big 'jumps' in the camera, adds distance to itself
+		tempPos.x = speed * Time.deltaTime;
+
+		//Accesses the Transform of the object this script is attached to
+		//Translate Moves the transform in the direction and distance of translation
+		// tempPos is the location data in the form of an (x,y,z) coordinate. Since nothing has been entered for the y and z, they default to 0.
+		// x has been defined in the statement above and changes every frame due to being in the update function
+		transform.Translate (tempPos);
+
+	}
+}
