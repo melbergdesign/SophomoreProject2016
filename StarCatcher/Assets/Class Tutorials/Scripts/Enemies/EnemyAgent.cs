faff88c34e7208818dda4273bc9c5966449516b0
Unknown <melbergdesign@gmail.com>
2016-10-10 13:55:37 -0600

    Beginning AI in Anthony's class

---
+++ StarCatcher/Assets/Class Tutorials/Scripts/Enemies/EnemyAgent.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAgent : MonoBehaviour {
+
+	//destination and agent variables needed
+	public NavMeshAgent myAgent;
+	public Transform player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Agents ignore speed from rest of unity
+		myAgent.destination = player.position;
+	
+	}
+}
---
+++ StarCatcher/Assets/Class Tutorials/Scripts/Enemies/EnemyController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+	
+	public CharacterController myCC;
+	public float gravity = 1f;
+	public Vector3 tempPOS;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		tempPOS.y -= gravity * Time.deltaTime;
+		myCC.Move (tempPOS);
+	}
+}
