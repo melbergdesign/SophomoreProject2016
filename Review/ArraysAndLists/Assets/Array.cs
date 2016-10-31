8312442c43fd96adf2f94084520c99e4794c6ed0
melberg <melbergdesign@gmail.com>
2016-10-31 15:38:52 -0600

    Arrays and Lists HomeworK
    Figure out how to get gameobjects into the list

--- Review/ArraysAndLists/Assets/Array.cs
+++ Review/ArraysAndLists/Assets/Array.cs
@@ -3,8 +3,21 @@ using System.Collections;
 
 public class Array : MonoBehaviour {
 
+	public int[] levelScores = {35, 16, 90, 12};
+
 	// Use this for initialization
 	void Start () {
+
+		levelScores [2] = 28;
+
+		for (int i = 0; i < levelScores.Length; i++)
+		{
+			int currentLevel = i+1;
+			//currentLevel can only be used in the for loop
+			print ("In level " + currentLevel + " you earned " + levelScores[i] + " points.");
+		}
+
+
 	
 	}
 	
--- Review/ArraysAndLists/Assets/List.cs
+++ Review/ArraysAndLists/Assets/List.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+//Lists namespace
+using System.Collections.Generic;
 
 public class List : MonoBehaviour {
 
+	public List<BuffItem> buffitems = new List<BuffItem> ();
+
 	// Use this for initialization
 	void Start () {
+		//add items from the scene to the list
+		buffitems.Add(GameObject.FindGameObjectsWithTag("Buff Item"));
 	
 	}
 	
